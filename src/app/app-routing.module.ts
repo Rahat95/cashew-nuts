dfc73c00cc177b329ea291da5f500f7206213425
Rahat <rahatpathan786.rp@gmail.com>
2023-11-30 10:36:28 +0530

    El-001:[ Header(navbar) done]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
+  {path:'user-login', component:LoginComponent}
 ];
 
 @NgModule({
