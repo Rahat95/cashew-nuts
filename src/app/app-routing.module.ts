a47a96f3004c97d74e114f33466490e990368040
Rahat <rahatpathan786.rp@gmail.com>
2023-11-30 12:05:15 +0530

    EL-001:[login and signup form created]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path:'user-login', component:LoginComponent}
+  {path:'user-login', component:LoginComponent},
+  {path:'user-signup', component:SignupComponent}
 ];
 
 @NgModule({
